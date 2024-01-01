1452eee0a0c69e2e2cb4e80c79cca8c86168fa38
Ali BENALI <daiguelali@gmail.com>
2024-01-01 18:34:47 +0100

    Create svconfig.lua

---
+++ config/svconfig.lua
@@ -0,0 +1,8 @@
+Logs = {
+    --|> 𝗗𝗜𝗦𝗖𝗢𝗥𝗗 𝗕𝗢𝗧 ----------------
+	Webhook = "https://discord.com/api/webhooks/1151568659379146892/pySlfEZo_4jtSgQ7E8vgjlpFCvaL_ymKEb1RSsNPzfqx-wwYviudCfxki4epCgFpwX9V",
+	ServerName = "Test Server RP",
+	IconURL = "https://cdn.discordapp.com/attachments/1111280529082417203/1111289712095801415/logo_1.png",
+	BotName = "Logs - JEWELRY ROB"
+	---------------------------------------------
+}
